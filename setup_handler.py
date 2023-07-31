5e0681407ee44f580b45599846e52c1f4033bf1f
dvarkless <amir911157@yandex.ru>
2023-07-31 17:34:31 +0300

    Add setup_handler.py

---
+++ setup_handler.py
@@ -0,0 +1,20 @@
+import logging
+import logging.handlers
+
+
+def get_handler():
+    # create rotating file handler with 3 files, each limited to 3 MB
+    handler = logging.handlers.RotatingFileHandler(
+        "log.log", maxBytes=3 * 1024 * 1024, backupCount=3
+    )
+    handler.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter(
+        "%(asctime)s in %(name)s: %(levelname)s MESSAGE:'%(message)s"
+    )
+
+    # add formatter to handler
+    handler.setFormatter(formatter)
+
+    return handler

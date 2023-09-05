3ebd51cb0801ccb42fc0fbb4f801b706e4584a6c
dvarkless <amir911157@yandex.ru>
2023-09-05 14:08:21 +0300

    Add docstring to the get_config

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ from yaml import safe_load
 
 
 def get_config(config_path) -> dict:
+    """
+        Returns the YAML config file as a dict 
+        Args:
+            config_path: str - path to the config
+        Return:
+            config: dict - YAML config as a dict
+    """
     config_path = Path(config_path)
     assert config_path.exists()
     assert config_path.suffix == '.yml'

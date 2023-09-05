3584a332cb9412e578c797570ab442523704a7d4
dvarkless <amir911157@yandex.ru>
2023-09-05 14:13:25 +0300

    Add docstrings for logger functions, remove unnessesary comments

--- setup_logger.py
+++ setup_logger.py
@@ -3,24 +3,36 @@ import logging.handlers
 
 
 def get_handler():
+    """
+        Return handler for a logger.
+        Change this function to reconfigure logs in every class
+        Returns:
+            handler: logging.Formatter
+    """
     # create rotating file handler with 3 files, each limited to 1 MB
     handler = logging.handlers.RotatingFileHandler(
         "log.log", maxBytes=1 * 1024 * 1024, backupCount=3
     )
     handler.setLevel(logging.DEBUG)
 
-    # create formatter
     formatter = logging.Formatter(
         "%(asctime)s in %(name)s: %(levelname)s MESSAGE:'%(message)s"
     )
 
-    # add formatter to handler
     handler.setFormatter(formatter)
 
     return handler
 
 
 def get_logger(name):
+    """
+        Returns the logger for the provided name.
+        Usually called as `get_logger(__name__)`
+        Args:
+            name: str - name of the class
+        Returns:
+            logger: logging.Logger
+    """
     logger = logging.getLogger(name)
     if not logger.hasHandlers():
         logger.setLevel(logging.INFO)
